1bc09cf4da773c8866c6a66740bbf905a0833c21
wutangclancee <s1623705@sms.ed.ac.uk>
2019-09-24 20:54:24 +0100

    Made Direction Enum more useful by holding the respective angles

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,6 +1,12 @@
 public enum Direction {
-    N, NNE, NE, NEE,
-    E, EES, ES, ESS,
-    S, SSW, SW, SWW,
-    W, WWN, WN, WNN
+    N(0.0), NNE(22.5), NE(45), NEE(67.5),
+    E(90), EES(112.5), ES(135.0), ESS(157.5),
+    S(180), SSW(202.5), SW(225.0), SWW(247.5),
+    W(270.0), WWN(292.5), WN(315.0), WNN(337.5);
+
+    public final double angle;
+
+    private Direction(double angle){
+        this.angle = Math.toRadians(angle);
+    }
 }
