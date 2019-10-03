da43b9daaff8cd8c0603aeb8a0487749a6e3e9d7
wutangclancee <s1623705@sms.ed.ac.uk>
2019-10-03 15:40:11 +0100

    Added builder inner class to Station.

--- src/main/java/uk/ac/ed/inf/powergrab/Station.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Station.java
@@ -1,5 +1,13 @@
 package uk.ac.ed.inf.powergrab;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+
 import static java.lang.Double.max;
 import static java.lang.Double.min;
 
@@ -9,10 +17,10 @@ public class Station {
     private double balance;
     private double power;
 
-    public Station(double latitude, double longitude, double balance, double power){
-        this.position = new Position(latitude, longitude);
-        this.balance = balance;
-        this.power = power;
+    public Station(StationBuilder builder){
+        this.position = new Position(builder.latitude, builder.longitude);
+        this.balance = builder.balance;
+        this.power = builder.power;
     }
 
     public void transferBalance(Agent agent){
@@ -23,5 +31,37 @@ public class Station {
         double totalBalance = balance + agent.getBalance();
         balance = max(0, totalBalance - 255.0);
         agent.setBalance(min(255.0, totalBalance));
+    }
+
+    public static class StationBuilder{
+
+        private double latitude;
+        private double longitude;
+        private double balance;
+        private double power;
+
+        public StationBuilder setLatitude(double latitude){
+            this.latitude = latitude;
+            return this;
+        }
+
+        public StationBuilder setLongitude(double longitude){
+            this.longitude = longitude;
+            return this;
+        }
+
+        public StationBuilder setBalance(double balance){
+            this.balance = balance;
+            return this;
+        }
+
+        public StationBuilder setPower(double power){
+            this.power = power;
+            return this;
+        }
+
+        public Station build(){
+            return new Station(this);
+        }
     }
 }
