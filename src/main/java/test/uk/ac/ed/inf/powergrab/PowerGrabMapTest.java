b8445db67ee345317b552d6cc0c268fcd1c0590a
wutangclancee <s1623705@sms.ed.ac.uk>
2019-10-05 22:13:18 +0100

    Added some PowerGrabMap tests

--- src/main/java/test/uk/ac/ed/inf/powergrab/PowerGrabMapTest.java
+++ src/main/java/test/uk/ac/ed/inf/powergrab/PowerGrabMapTest.java
@@ -6,22 +6,35 @@ import uk.ac.ed.inf.powergrab.PowerGrabMap;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 
 public class PowerGrabMapTest {
 
     @Test
-    public void readStationListFromJson() throws IOException {
+    public void ifProvideLocalDateToGetMap_thenPowerGrabMapWith50StationsReturned() throws IOException {
 
-        PowerGrabMap powerGrabMap = PowerGrabMap.getMap(new File("/Users/mclancy/IdeaProjects/PowerGrab/.json_cache/2019/01/01/powergrabmap.geojson"));
-
-        //StationList list = (StationList) new ArrayList<Station>();
+        PowerGrabMap powerGrabMap = PowerGrabMap.getMap(1, 1, 2019);
+        assertCorrectAndNumStations50(powerGrabMap, 1, 1, 2019);
     }
 
     @Test
-    public void readStationListFromURL() throws IOException {
+    public void ifProvideFileObjectToGetMap_thenPowerGrabMapWith50StationsReturned() throws IOException {
 
-        PowerGrabMap powerGrabMap = PowerGrabMap.getMap(new URL("http://homepages.inf.ed.ac.uk/stg/powergrab/2020/01/01/powergrabmap.geojson"));
-
+        PowerGrabMap powerGrabMap = PowerGrabMap.getMap(new File("/Users/mclancy/IdeaProjects/PowerGrab/.json_cache/2019/01/01/powergrabmap.geojson"));
+        assertCorrectAndNumStations50(powerGrabMap, 1, 1, 2019);
     }
+
+    @Test
+    public void ifProvideURLObjectToGetMap_thenPowerGrabWith50StationReturned() throws IOException {
+
+        PowerGrabMap powerGrabMap = PowerGrabMap.getMap(new URL("http://homepages.inf.ed.ac.uk/stg/powergrab/2019/01/01/powergrabmap.geojson"));
+        assertCorrectAndNumStations50(powerGrabMap, 1, 1, 2019);
+    }
+
+    private void assertCorrectAndNumStations50(PowerGrabMap powerGrabMap, int day, int month, int year) {
+        assert powerGrabMap.getDateGenerated().equals(LocalDate.of(year, month, day));
+        assert powerGrabMap.getNumStations() == 50;
+    }
+
 }
 
