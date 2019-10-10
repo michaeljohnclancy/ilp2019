f3ecd06dfd04a803f7d3b65407fcb351e86760ee
wutangclancee <s1623705@sms.ed.ac.uk>
2019-10-10 17:04:04 +0100

    Moved playArea static definition into Position. Added javadoc.

--- src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -1,8 +1,16 @@
 package uk.ac.ed.inf.powergrab;
 
-import static uk.ac.ed.inf.powergrab.App.playArea;
+import java.awt.geom.Rectangle2D;
 
 public class Position {
+
+
+    private static final double LATMIN = 55.942617;
+    private static final double LATMAX = 55.946233;
+    private static final double LONGMIN = -3.192473;
+    private static final double LONGMAX = -3.184319;
+
+    private static final Rectangle2D.Double playArea = new Rectangle2D.Double(LATMIN, LONGMIN, LATMAX-LATMIN, LONGMAX-LONGMIN);
 
     private static final double R = 0.0003;
 
@@ -15,6 +23,11 @@ public class Position {
         this.longitude = longitude;
     }
 
+    /**
+     * This method returns a new Position given a direction. It accesses the precalculated sin and cos values of the given direction.
+     * @param direction Direction from the current position to travel in.
+     * @return Updated position after moving in given direction.
+     */
     public Position nextPosition(Direction direction){
         return new Position(
                 latitude - (R * direction.sinAngle),
@@ -22,8 +35,12 @@ public class Position {
         );
     }
 
+    /**
+     * This method calls 'contains' method of the static Rectangle2D object defined in App. This returns a boolean value
+     * indicating whether or a given latitude and longitude is within the play area.
+     * @return
+     */
     public boolean inPlayArea() {
         return playArea.contains(latitude, longitude);
-
     }
 }
